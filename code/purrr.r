321bf06ae12af9be574a78520f01cbb2d22e3480
Jared Lander <info@jaredlander.com>
2018-06-21 13:09:26 -0400

    Showed map2

--- code/purrr.r
+++ code/purrr.r
@@ -34,3 +34,19 @@ theList %>% map_dbl(mean) %>% sum()
 
 theList %>% map(class)
 theList %>% map_chr(class)
+
+secondList <- list(10:1, 5, c(1, 4, 2), 6:8)
+secondList
+
+addSums <- function(x, y)
+{
+    return(sum(x) + sum(y))
+}
+
+addSums(1:10, c(1, 4, 3))
+
+addSums(theList[[1]], secondList[[1]])
+addSums(theList[[2]], secondList[[2]])
+
+map2(theList, secondList, addSums)
+map2_dbl(theList, secondList, addSums)
