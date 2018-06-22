5fb4a6d4f8886e4d8432c348a2f42d10e54d08f7
Jared Lander <info@jaredlander.com>
2018-06-22 11:38:39 -0400

    Showed dplyr in database

---
+++ code/dplyr_database.r
@@ -0,0 +1,35 @@
+library(dplyr)
+library(DBI)
+
+con <- dbConnect(RSQLite::SQLite(), 'data/diamonds.db')
+con
+class(con)
+
+dbListTables(con)
+dbListFields(con, 'diamonds')
+
+diaTab <- tbl(con, 'diamonds')
+diaTab
+nrow(diaTab)
+class(diaTab)
+pryr::object_size(diaTab)
+
+diaTab %>% select(carat, price)
+diaTab %>% filter(cut == 'Ideal')
+
+diaTab %>% mutate(Ratio=price/carat)
+diaTab
+
+diaTab %>% 
+    group_by(cut) %>% 
+    summarize(Price=mean(price))
+
+diaTab %>% 
+    group_by(cut) %>% 
+    summarize(Price=AVG(price))
+
+dbplyr::translate_sql(
+    diaTab %>% 
+        group_by(cut) %>% 
+        summarize(Price=AVG(mean))
+)
